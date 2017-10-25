67ca87d277ff5aaf8c042315d4c6b6d1f8bc0128
Faïssal <faissal.hattou@gmail.com>
2017-10-25 10:33:25 +0200

    TO DO: 2 last panels

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 // flex panels
-
 const panels = (e) => {
   tar = e.target.parentNode;
   if (tar.style.flex == "1 1 0%") {
-    tar.style.flex = "12";
-  }
-  else {
+    tar.style.flex = "10";
+  } else {
     tar.style.flex = "1";
   }
-  console.log(tar);
   tar.children['1'].children['0'].classList.toggle('open');
 }
 
 const arr = Array.from(document.getElementById('flex-panels').children);
 arr.forEach(box => box.addEventListener('click', panels));
+
+// jquery
+$(document).ready(function() {
+  // presentation
+  $('#presentation-box').click(function() {
+    $('.title').toggleClass('display');
+    $('.presentation').toggle(400);
+  });
+// skills
+  $('#skills-box').click(function() {
+    $('.title').toggleClass('display');
+    $('.skills').toggle(400);
+  });
+  // projects
+  $('#projects-box').click(function() {
+    $('.title').toggleClass('display');
+    $('.projects').toggle(400);
+  });
+});
